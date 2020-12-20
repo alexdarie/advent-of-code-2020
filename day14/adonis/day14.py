c446668a15b01c183281e9183b5ef695049cf638
Adonis Bodea <31371688+adonisbodea02@users.noreply.github.com>
2020-12-20 12:21:56 +0100

    day14 part2

--- day14/adonis/day14.py
+++ day14/adonis/day14.py
@@ -46,6 +46,44 @@ def apply_mask_to_number(mask, number):
             
     return new_number
 
+
+def apply_mask_to_memory_address(mask, memory_address):
+	new_memory_address = ''
+	
+	for i in range(36):
+		if mask[i] == 'X' or mask[i] == '1':
+			new_memory_address = new_memory_address + mask[i]
+		else:
+			new_memory_address = new_memory_address + memory_address[i]
+
+	return new_memory_address
+
+
+def generate_memory_addresses(mask, memory_address):
+	memory_addresses = [apply_mask_to_memory_address(mask, memory_address)]
+	
+	ok = False
+	
+	while not ok:
+	
+		current_memory_address = memory_addresses.pop(0)
+		floating_bit = current_memory_address.find('X')
+		if floating_bit != -1:
+			memory_addresses.append(current_memory_address[:floating_bit] + '1' + current_memory_address[floating_bit + 1:])
+			memory_addresses.append(current_memory_address[:floating_bit] + '0' + current_memory_address[floating_bit + 1:])
+		else:
+			memory_addresses.append(current_memory_address)
+			ok = True
+			i = 0
+			while i < len(memory_addresses):
+				if memory_addresses[i].find('X') != -1:
+					ok = False
+					i = len(memory_addresses)
+				else:
+					i += 1
+	
+	return memory_addresses
+	
     
 def convert_36bit_representation_to_number(representation):
     number = 0
@@ -54,7 +92,7 @@ def convert_36bit_representation_to_number(representation):
             number += 2 ** (36 - i)
             
     return number
-    
+
  
 def task1(commands):
     mask = ""
@@ -75,8 +113,29 @@ def task1(commands):
         
     return values_left_in_memory
 
+
+def task2(commands):
+    mask = ""
+    memory_altered = {}
     
-print(task1(read_data("input_day14.txt")))
+    for command in commands:
+        if type(command) is str:
+            mask = command
+        else:
+            representation = convert_number_to_36bit_representation(command[0])
+            memory_addresses = generate_memory_addresses(mask, representation)
+            for memory_address in memory_addresses:
+                masked_memory_address = convert_36bit_representation_to_number(memory_address)
+                memory_altered[masked_memory_address] = command[1]
+    
+    values_left_in_memory = 0
+    for memory_address in memory_altered.keys():
+        values_left_in_memory += memory_altered[memory_address]
+        
+    return values_left_in_memory
+
+    
+print(task2(read_data("input_day14.txt")))
 
 
 
